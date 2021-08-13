f17866d8440b4c6488a79f6146a5003b3836d35c
yegon <yegon.johnstone@student.moringaschool.com>
2021-08-13 16:36:28 +0300

    create project sms

--- smsproject/settings.py
+++ smsproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost', 'b3f5365221b2.ngrok.io']
+ALLOWED_HOSTS = ['127.0.0.1','localhost', 'b7f73d374cc4.ngrok.io']
 
 
 # Application definition
